bbc021c7443834ff30d838413abff4ab89fdecba
takanori ootake <mine19876293@gmail.com>
2023-10-03 19:20:48 +0900

    php03

---
+++ src/php01/index02.php
@@ -0,0 +1,10 @@
+<?php
+$a = 15;
+$b = 3;
+$c = 10;
+
+$answer1 = $a + $b;
+$answer2 = $a - $b;
+$answer3 = $a * $b;
+$answer4 = $a - $c;
+$answer5 = $
---
+++ src/php01/index03.php
@@ -0,0 +1,8 @@
+<?php
+$a = 20;
+$b = 5;
+
+
+echo ($a > $b;)
+echo "<br />";
+echo $c;
